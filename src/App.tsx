5ac81e5eb428a42dff5de24e2bfead9a11e8dfbc
viviana-aline <30525528+viviana-aline@users.noreply.github.com>
2021-04-18 02:40:11 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,32 @@ const INITIAL_CELLS = new Array(15).fill(new Array(15).fill(null));
 export default function App() {
   const [playerTurn, setPlayerTurn] = useState<number>(0);
   const [cells, setCells] = useState<string[][]>(INITIAL_CELLS);
-  const [selectedTile, setSelectedTile] = useState("A");
+  const [currentSelection, setCurrentSelection] = useState({
+    playerTile: "",
+    boardPos: []
+  });
+
+  function onCellSelect(row, column) {
+    setCurrentSelection((prev) => ({ ...prev, boardPos: [row, column] }));
+    console.log(currentSelection);
+  }
+
+  function onTileSelect(selectedTile) {
+    setCurrentSelection((prev) => ({ ...prev, playerTile: selectedTile }));
+    console.log(currentSelection);
+  }
 
   return (
     <div className="App">
       <p>It's player {playerTurn}'s turn</p>
-      <Board cells={cells} />
+      <Board cells={cells} onCellSelect={onCellSelect} />
 
       <Racks>
-        <Rack player={1} selectedTile={selectedTile} setSelectedTile={} />
+        <Rack
+          player={1}
+          selectedTile={currentSelection.playerTile}
+          onTileSelect={onTileSelect}
+        />
         <Rack player={2} />
       </Racks>
     </div>
