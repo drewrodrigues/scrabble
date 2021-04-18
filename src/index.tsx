ddab7da153952ae47e8e44b0239617fbd86d2326
Visard\Pavel <fearoo90@gmail.com>
2021-04-19 00:32:39 +0300

    Fix TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
+import React from 'react';
 import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import App from "./App";
 import Board from "./components/board";
 
 const rootElement = document.getElementById("root");
 render(
-  <Router>
-    <Switch>
-      <Route to="/">
-        <App />
-      </Route>
-      <Route to="/drew">
-        <Board />
-      </Route>
-    </Switch>
-  </Router>,
+  <App />,
   rootElement
 );
