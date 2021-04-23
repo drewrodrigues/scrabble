3835a7e025a98504e37245372ca37d3449e04d5d
Viviana <viviana@motionsquad.de>
2021-04-23 23:54:06 +0300

    Fix PR comment

--- src/components/rack.tsx
+++ src/components/rack.tsx
@@ -5,17 +5,17 @@ import "./rack.scss";
 interface RackProps {
   player: number;
   tiles: string[];
-  selectedTile: number | undefined;
+  selectedTileIndex: number | undefined;
   onTileSelect: (selectedTile: number) => void;
 }
 
-export function Rack({ player, tiles, selectedTile, onTileSelect }: RackProps) {
+export function Rack({ player, tiles, selectedTileIndex, onTileSelect }: RackProps) {
   return (
     <div className="rack">
       {tiles.map((tile, index) => (
         <div
           key={index}
-          className={classNames("playerTile", { "selectedTile": selectedTile === index })}
+          className={classNames("playerTile", { "selectedTile": selectedTileIndex === index })}
           onClick={() => onTileSelect(index)}
         >
           {tile}
