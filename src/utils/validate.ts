2db2284194a50635070b4c4be167ee8df22c4567
Drew Rodrigues <thesimpledev@gmail.com>
2021-04-23 13:28:38 -0700

    Add most validators

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,5 +1,15 @@
 import { Cells, CurrentTurn } from "../App";
 
+enum Direction {
+  Vertical = "Vertical",
+  Horizontal = "Horizontal",
+}
+
+export default function validate(cells: Cells, currentTurn: CurrentTurn[]) {
+  _validateCenterCovered(cells, currentTurn);
+  _validateCurrentTurnAdjacent(currentTurn);
+}
+
 function _validateCenterCovered(cells: Cells, currentTurn: CurrentTurn[]) {
   // TODO: @drew make flexible for different sized boards
   const isCenterCovered =
@@ -11,8 +21,44 @@ function _validateCenterCovered(cells: Cells, currentTurn: CurrentTurn[]) {
   }
 }
 
-function _validateMoves(cells: Cells, currentTurn: CurrentTurn[]) {}
+function _validateCurrentTurnAdjacent(currentTurn: CurrentTurn[]) {
+  if (currentTurn.length === 1) return;
+  const direction =
+    currentTurn[0].column == currentTurn[1].column
+      ? Direction.Vertical
+      : Direction.Horizontal;
 
-export default function validate(cells: Cells, currentTurn: CurrentTurn[]) {
-  _validateCenterCovered(cells, currentTurn);
+  _verifyConcurrentDirection(direction, currentTurn);
+}
+
+// we iterate from min to max to make sure each is present
+function _verifyConcurrentDirection(
+  direction: Direction,
+  cells: CurrentTurn[]
+) {
+  const rowOrColumnValues = cells.map(
+    (cell) => cell[direction === Direction.Vertical ? "row" : "column"]
+  );
+  const [min, max] = [
+    Math.min(...rowOrColumnValues),
+    Math.max(...rowOrColumnValues),
+  ];
+
+  for (let i = min; i <= max; i++) {
+    if (!rowOrColumnValues.includes(i)) {
+      throw new Error("There's a gap in the cells direction");
+    }
+  }
+
+  const shouldBeStaticValue = cells.map(
+    (cell) => cell[direction === Direction.Vertical ? "column" : "row"]
+  );
+  const allStaticValuesEqual = shouldBeStaticValue.every(
+    (value) => value === shouldBeStaticValue[0]
+  );
+  if (!allStaticValuesEqual) {
+    throw new Error(
+      `Cells are not concurrently ${direction} and they are expected to be`
+    );
+  }
 }
