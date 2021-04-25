55f7a7e06f594f853888224ac3cea5cadab443f1
viviana-aline <viviana@motionsquad.de>
2021-04-26 02:37:13 +0300

    Fix PR comments

---
+++ src/components/board/tile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { TileType } from "../../utils/tiles";
+
+interface TileProps {
+  tile: TileType;
+}
+
+export function Tile({ tile }: TileProps) {
+  const { letter, points } = tile;
+
+  return (
+    <div className="tile">
+      <div className="tile-letter">{letter}</div>
+      <span className="tile-points">{points}</span>
+    </div>
+  );
+}
